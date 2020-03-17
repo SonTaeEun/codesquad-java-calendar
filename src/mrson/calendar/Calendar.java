f5ce0ab49136cd8b8b2079de3ef5f35980cf92f3
SonTaeEun <mrson362@naver.com>
2020-03-17 18:04:30 +0900

    add maxDays

--- src/mrson/calendar/Calendar.java
+++ src/mrson/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package mrson.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 	public static void main(String[] args) {
 		System.out.println("일 월  화  수  목  금  토");
@@ -8,5 +10,18 @@ public class Calendar {
 		 System.out.println(" 8  9  10  11  12  13 14");
 		 System.out.println("15 16 17  18  19  20 21");
 		 System.out.println(" 22 23 24  25  26 27 28");
+		 
+		 //입력 받은 달의 최대 일수 출력
+		 int inputMonth;
+		 Scanner scan = new Scanner(System.in);
+		 
+		 System.out.print("출력을 원하는 달을 입력하세요 : ");
+		 inputMonth = scan.nextInt();
+		 
+		 int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		 
+		 System.out.printf("%d 월은 %d 까지 있습니다.", inputMonth, maxDays[inputMonth-1]);
+		 
+		 scan.close();
 	}
 }
